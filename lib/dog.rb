2f666244ba94ecb14d5f846bc4e837780bdb5b6a
phajib <phajib@gmail.com>
2019-02-28 02:34:19 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    #attr_accessor :breed, :dog
+
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    #def breed=(breed)
+    #    @breed = breed
+    #  end
+     
+    #def breed
+    #    @breed
+    #end 
+end
